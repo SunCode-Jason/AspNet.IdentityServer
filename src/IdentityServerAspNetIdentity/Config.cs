e8758f9d178fadb473dda8c58f957f026d6b288f
Jason <2900317820@qq.com>
2024-11-29 14:20:11 +0800

    使用AspNetIndentity模板

---
+++ src/IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,104 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdentityServerAspNetIdentity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        [
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            /*
+                定义名称为：email的资源
+                客户端通过scope值， 获取关联的 UserClaims
+             */
+            new IdentityResource(){
+                Name = "email", // 
+                UserClaims = [
+                JwtClaimTypes.Email,
+                JwtClaimTypes.EmailVerified
+                ]
+            }
+        ];
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        [
+            new ApiScope("scope1"),
+            new ApiScope("scope2"),
+            new ApiScope(name: "api1", displayName: "My API")
+        ];
+
+    public static IEnumerable<Client> Clients =>
+        [
+            // machine to machine client (from quickstart 1)
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            },
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                // 允许客户端刷新令牌
+                AllowOfflineAccess = true,
+                // 指定客户端可访问的资源
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "email",
+                    "api1"
+                }
+            },
+            // m2m client credentials flow client
+            new Client
+            {
+                ClientId = "m2m.client",
+                ClientName = "Client Credentials Client",
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                AllowedScopes = { "scope1" }
+            },
+
+            // interactive client using code flow + pkce
+            new Client
+            {
+                ClientId = "interactive",
+                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                RedirectUris = { "https://localhost:44300/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "scope2" }
+            },
+        ];
+}
